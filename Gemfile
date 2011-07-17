f6436ccc914983687d676e46fd99f0f8a8433e9e
Joe Yates <joe.g.yates@gmail.com>
2011-07-17 12:26:04 +0100

    Added HAML views

--- Gemfile
+++ Gemfile
@@ -5,6 +5,7 @@ gem 'ruboto-core'
 gem 'nokogiri'
 gem 'sqlite3'
 gem 'activerecord'
+gem 'haml'
 
 group :development do
   gem 'mongrel', '1.2.0.pre2'
--- lib/ruboto/api/app.rb
+++ lib/ruboto/api/app.rb
@@ -1,24 +1,21 @@
 #!/usr/bin/env ruby
 
 require 'sinatra/base'
-require 'ruboto'
-require 'ruboto/api'
-require 'ruboto/util/scan_in_api'
-require 'ruboto/util/xml_element'
-
-=begin
-
-
-=end
-
-
+require 'haml'
 
 class RubotoApiApp < Sinatra::Base
 
+  set :root, File.join( File.dirname( __FILE__ ), 'app' )
+
+  get '/:package' do
+    @package = Ruboto::Api::Package.find_by_name( params[ :package ] )
+    haml :package
+  end
+
   get '/:package/:klass' do
-    package = Ruboto::Api::Package.find_by_name( params[ :package ] )
-    klass   = package.klasses.find_by_name( params[ :klass ] )
-    klass.name
+    @package = Ruboto::Api::Package.find_by_name( params[ :package ] )
+    @klass   = @package.klasses.find_by_name( params[ :klass ] )
+    haml :klass
   end
 
 end
